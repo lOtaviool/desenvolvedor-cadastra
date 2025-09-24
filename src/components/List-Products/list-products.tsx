0bf3eac659576cb622c257cbc5215f2d520d84a9
Luis Otávio <luisotavio95@gmail.com>
2025-09-23 21:28:59 -0300

    Criação do layout mobile

--- src/components/List-Products/list-products.tsx
+++ src/components/List-Products/list-products.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
-import { useGetProducts } from "../../hooks/products.hook";
+import { useGetProducts } from "../../hooks/use-product";
 import { Product } from "../../ts/Product";
 import Filters from "../Filters/filters";
 import ProductCard from "../Product-Card/product-card";
 import "./list-products.scss";
+import { useMobile } from "../../hooks/use-mobile";
 
 export default function ListProducts() {
   const { data, isLoading, isError } = useGetProducts();
+  const isMobile = useMobile();
   const [visibleCount, setVisibleCount] = useState(9);
   const increment = 3;
+
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   const [sortOrder, setSortOrder] = useState<
     "Mais recente" | "Menor Preço" | "Maior Preço" | ""
@@ -41,26 +44,90 @@ export default function ListProducts() {
     <div className="container">
       <div className="header-list">
         <span>Blusas</span>
-        <div>
-          <select
-            value={sortOrder}
-            onChange={(e) =>
-              setSortOrder(
-                e.target.value as
-                  | "Mais recente"
-                  | "Menor Preço"
-                  | "Maior Preço"
-                  | ""
-              )
-            }
-          >
-            <option value="">Selecione</option>
-            <option value="Mais recente">Mais recente</option>
-            <option value="Menor Preço">Menor Preço</option>
-            <option value="Maior Preço">Maior Preço</option>
-          </select>
-        </div>
+
+        {isMobile ? (
+          <div className="mobile-bar mobile-actions">
+            <button
+              onClick={() => document.body.classList.add("drawer-filters-open")}
+            >
+              Filtrar
+            </button>
+            <button
+              onClick={() => document.body.classList.add("drawer-order-open")}
+            >
+              Ordenar
+            </button>
+          </div>
+        ) : (
+          <div className="order-by">
+            <label htmlFor="sortOrder">Ordenar por:</label>
+            <select
+              id="sortOrder"
+              value={sortOrder}
+              onChange={(e) =>
+                setSortOrder(
+                  e.target.value as
+                    | "Mais recente"
+                    | "Menor Preço"
+                    | "Maior Preço"
+                    | ""
+                )
+              }
+            >
+              <option value="">--</option>
+              <option value="Mais recente">Mais recente</option>
+              <option value="Menor Preço">Menor Preço</option>
+              <option value="Maior Preço">Maior Preço</option>
+            </select>
+          </div>
+        )}
       </div>
+
+      {isMobile && (
+        <div>
+          <div className="drawer drawer-filters">
+            <button
+              className="close"
+              onClick={() =>
+                document.body.classList.remove("drawer-filters-open")
+              }
+            >
+              x
+            </button>
+            <h3>Filtros</h3>
+            <Filters products={data} setProducts={setFilteredProducts} />
+          </div>
+
+          <div className="drawer drawer-order">
+            <button
+              className="close"
+              onClick={() =>
+                document.body.classList.remove("drawer-order-open")
+              }
+            >
+              x
+            </button>
+            <h3>Ordenar por</h3>
+            <select
+              value={sortOrder}
+              onChange={(e) =>
+                setSortOrder(
+                  e.target.value as
+                    | "Mais recente"
+                    | "Menor Preço"
+                    | "Maior Preço"
+                    | ""
+                )
+              }
+            >
+              <option value="">--</option>
+              <option value="Mais recente">Mais recente</option>
+              <option value="Menor Preço">Menor Preço</option>
+              <option value="Maior Preço">Maior Preço</option>
+            </select>
+          </div>
+        </div>
+      )}
 
       <div className="content-list">
         <div className="filters-container">
@@ -68,9 +135,17 @@ export default function ListProducts() {
         </div>
 
         <div className="list-data">
-          {visibleProducts.map((product: Product, index) => (
-            <ProductCard key={index} product={product} onClickBuy={() => {}} />
-          ))}
+          {visibleProducts.length > 0 ? (
+            visibleProducts.map((product: Product, index) => (
+              <ProductCard
+                key={index}
+                product={product}
+                onClickBuy={() => {}}
+              />
+            ))
+          ) : (
+            <h1>Nenhum produto encontrado! :(</h1>
+          )}
         </div>
       </div>
 
@@ -82,7 +157,7 @@ export default function ListProducts() {
             className="load-more"
             onClick={() => setVisibleCount((prev) => prev + increment)}
           >
-            Carregar mais
+            CARREGAR MAIS
           </button>
         </div>
       )}
